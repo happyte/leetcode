1a61f1fb0a2805e393a7bbe6840cf1e9d7d0d192
happyte <1016917515@qq.com>
2017-06-12 23:40:32 +0800

    判断链表是否有环

---
+++ src/linkedListCycle/Solution.java
@@ -0,0 +1,25 @@
+package linkedListCycle;
+
+public class Solution {
+	class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) {
+		  val = x;
+		  next = null;
+		}
+	}
+	public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null)
+            return false;
+        ListNode worker = head;
+        ListNode runner = head;
+        while(runner!= null && runner.next != null && runner.next.next != null){
+            worker = worker.next;
+            runner = runner.next.next;
+            if(worker == runner)
+                return true;
+        }
+        return false;
+    }
+}
