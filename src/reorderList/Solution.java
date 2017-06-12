4a2f78fc16cc94732f59472d679f33a2079b829f
happyte <1016917515@qq.com>
2017-06-12 23:32:54 +0800

    链表插入的一种特殊情况，例如1,2,3,4,5,6变为1,6,2,5,3,4

---
+++ src/reorderList/Solution.java
@@ -0,0 +1,64 @@
+package reorderList;
+
+public class Solution {
+	class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) {
+		   val = x;
+		   next = null;
+		}
+	}
+	public void reorderList(ListNode head) {
+        if(head == null || head.next==null)  
+    	{  
+        	return;  
+    	}  
+    	ListNode walker = head;  
+    	ListNode runner = head;  
+    	while(runner.next!=null && runner.next.next!=null)  
+    	{  
+        	walker = walker.next;  
+        	runner = runner.next.next;  
+    	}  
+    	ListNode head1 = head;  
+    	ListNode head2 = walker.next;  
+    	walker.next = null;  
+    	head2 = recursive_reverse(head2);  
+    	while(head1!=null && head2!=null)  
+    	{  
+        	ListNode next = head2.next;  
+        	head2.next = head1.next;  
+        	head1.next = head2;  
+        	head1 = head2.next;  
+        	head2 = next;  
+    	}  
+    }
+	//非递归方式反转链表
+	private ListNode reverse(ListNode head){
+		ListNode pre = null;
+		ListNode cur = head;
+		ListNode next = head.next;
+		while(cur != null){
+			next = cur.next;
+			cur.next = pre;
+			pre = cur;
+			cur = next;
+		}
+		return pre;
+	}
+	//递归方式的反转链表
+	public ListNode recursive_reverse(ListNode head) {  
+    	if(head == null || head.next==null)  
+        	return head;  
+    	return recursive_reverse(head, head.next);  
+	}
+    private ListNode recursive_reverse(ListNode current, ListNode next)   
+	{  
+    	if (next == null) return current;  
+    	ListNode newHead = recursive_reverse(current.next, next.next);  
+    	next.next = current;  
+    	current.next = null;  
+    	return newHead;  
+	}  
+}
