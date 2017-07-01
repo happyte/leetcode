0e59579b7c2092470e4432e99a9e7e4de112a628
happyte <1016917515@qq.com>
2017-07-01 19:22:15 +0800

    删除链表中重复的节点，与剑指offer的那题不同，这题还保留了一个重复的节点

---
+++ src/deleteDuplication/Solution.java
@@ -0,0 +1,44 @@
+package deleteDuplication;
+
+import dataStructure.ListNode;
+
+public class Solution {
+	public static ListNode deleteDuplication(ListNode pHead)
+    {    
+        if (pHead == null) 
+        	return null;
+        ListNode helper = new ListNode(0);
+        helper.next = pHead;
+        ListNode p = pHead;
+        while(p != null){
+        	ListNode q = p.next;
+        	if(q == null) break;
+        	//p,q两者值相同
+        	if(p.val == q.val){
+        		while(q != null && q.val == p.val){
+            		q = q.next;
+            	}
+        		p.next = q;
+        	}
+        	p = q;
+        }
+        return helper.next;
+    }
+	
+	public static void main(String[] args) {
+		ListNode l1 = new ListNode(1);
+		ListNode l2 = new ListNode(1);
+		ListNode l3 = new ListNode(2);
+		ListNode l4 = new ListNode(3);
+		ListNode l5 = new ListNode(3);
+		l1.next = l2;
+		l2.next = l3;
+		l3.next = l4;
+		l4.next = l5;
+		ListNode head = deleteDuplication(l1);
+		while(head != null){
+			System.out.println(head.val);
+			head = head.next;
+		}
+	}
+}
