b7e9e72b9c569ad0a9259d2efdaa37fe082ac535
happyte <1016917515@qq.com>
2017-07-05 20:49:29 +0800

    1.数组中没有重复元素 2.数组中有重复元素   2的技巧在于记录了上一次的size大小，如果前后两者元素相同，则不从0开始，而直接从上次的size开始

---
+++ src/subSets/Solution.java
@@ -0,0 +1,64 @@
+package subSets;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+	public static ArrayList<ArrayList<Integer>> subsets(int[] S) {
+		ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        if(S == null || S.length == 0)
+        	return res;
+        ArrayList<Integer> item = new ArrayList<>();
+        res.add(item);
+        Arrays.sort(S); //从小到大排序
+        for(int i=0;i<S.length;i++){
+        	int size = res.size();
+        	for(int j=0;j<size;j++){
+        		//非递归方式，添加顺序为[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]
+        		item = new ArrayList<>(res.get(j));
+        		item.add(S[i]);
+        		res.add(item);
+        	}
+        }
+        return res;
+    }
+	
+	public static ArrayList<ArrayList<Integer>> subsets2(int[] S){
+        if(S == null)
+        	return null;
+        Arrays.sort(S);
+        return helper(S, S.length-1);
+	}
+	
+	private static ArrayList<ArrayList<Integer>> helper(int[] S,int len){
+		if(len == -1){
+			ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+			ArrayList<Integer> item = new ArrayList<>();
+			res.add(item);
+			return res;
+		}
+		//先递归找到第一个[]
+		ArrayList<ArrayList<Integer>> res = helper(S, len-1);
+		int size = res.size();
+		for(int i=0;i<size;i++){
+			ArrayList<Integer> item = new ArrayList<>(res.get(i));
+			item.add(S[len]);
+			res.add(item);
+		}
+		return res;
+	}
+	
+	
+	public static void main(String[] args) {
+		int[] A = new int[]{1,2,3};
+		ArrayList<ArrayList<Integer>> res = subsets(A);
+		System.out.println(res.size());
+		for(int i=0;i<res.size();i++){
+			for(int j=0;j<res.get(i).size();j++){
+				System.out.print(res.get(i).get(j));
+			}
+			System.out.println("");
+		}
+	}
+}
---
+++ src/subSetsII/Solution.java
@@ -0,0 +1,76 @@
+package subSetsII;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Solution {
+	public static ArrayList<ArrayList<Integer>> subsets(int[] S) {
+		ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        if(S == null || S.length == 0)
+        	return res;
+        ArrayList<Integer> item = new ArrayList<>();
+        res.add(item);
+        Arrays.sort(S); //从小到大排序
+        int start = 0;
+        for(int i=0;i<S.length;i++){
+        	int size = res.size();
+        	for(int j=start;j<size;j++){
+        		//非递归方式，添加顺序为[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]
+        		item = new ArrayList<>(res.get(j));
+        		item.add(S[i]);
+        		res.add(item);
+        	}
+        	if(i<S.length-1 && S[i] == S[i+1]){
+        		start = size; //上一次的大小
+        	}
+        	else{
+        		start = 0;
+        	}
+        }
+        return res;
+    }
+	
+	public static ArrayList<ArrayList<Integer>> subsets2(int[] S){
+        if(S == null)
+        	return null;
+        Arrays.sort(S);
+        ArrayList<Integer> lastIndex = new ArrayList<>();
+        lastIndex.add(0);
+        return helper(S, S.length-1,lastIndex);
+	}
+	
+	private static ArrayList<ArrayList<Integer>> helper(int[] S,int len,ArrayList<Integer> lastIndex){
+		if(len == -1){
+			ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+			ArrayList<Integer> item = new ArrayList<>();
+			res.add(item);
+			return res;
+		}
+		//先递归找到第一个[]
+		ArrayList<ArrayList<Integer>> res = helper(S, len-1,lastIndex);
+		int size = res.size();
+		int start = 0;
+		if(len > 0 && S[len] == S[len-1])
+			start = lastIndex.get(0);
+		for(int i=start;i<size;i++){
+			ArrayList<Integer> item = new ArrayList<>(res.get(i));
+			item.add(S[len]);
+			res.add(item);
+		}
+		lastIndex.set(0,size);
+		return res;
+	}
+	
+	
+	public static void main(String[] args) {
+		int[] A = new int[]{1,2,2};
+		ArrayList<ArrayList<Integer>> res = subsets2(A);
+		System.out.println(res.size());
+		for(int i=0;i<res.size();i++){
+			for(int j=0;j<res.get(i).size();j++){
+				System.out.print(res.get(i).get(j));
+			}
+			System.out.println("");
+		}
+	}
+}
