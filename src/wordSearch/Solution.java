68210edb626edfa8a1c9fecadaf07acf16d19137
happyte <1016917515@qq.com>
2017-07-05 17:37:55 +0800

    wordserach 对每个点使用dos搜索，复杂度最差为O(m*m*n*n),其中used二维数组用来维护dfs搜索过程中经过的点，然后保护现场，递归函数执行完后，uesd二维数组恢复成全为false

---
+++ src/wordSearch/Solution.java
@@ -0,0 +1,39 @@
+package wordSearch;
+
+
+public class Solution {
+	public static boolean exist(char[][] board, String word) {
+		 if(board == null || board.length == 0 || word == null || word.length() == 0)
+			 return false;
+		 //维护这个二维数组,在dfs函数中保护现场，只是维护在dfs搜索中，防止重复搜索
+		 boolean[][] used = new boolean[board.length][board[0].length];
+		 for(int i=0;i<board.length;i++){
+			 for(int j=0;j<board[i].length;j++)
+				 if(dfs(board, word, 0, i, j, used))
+					 return true;
+		 }
+		 return false;
+	}
+	
+	private static boolean dfs(char[][] board,String word,int index,int i,int j,boolean[][] used){
+		if(index == word.length())
+			return true;
+		if(i<0 || j<0 || i>=board.length || j>=board[i].length || used[i][j] || board[i][j]!=word.charAt(index))
+			return false;
+		used[i][j] = true;
+		boolean res = dfs(board, word, index+1, i-1, j, used) || 
+					  dfs(board, word, index+1, i+1, j, used) ||
+					  dfs(board, word, index+1, i, j-1, used) ||
+					  dfs(board, word, index+1, i, j+1, used);
+		used[i][j] = false;
+		return res;
+	}
+	
+	public static void main(String[] args) {
+		char[][] A = new char[][]{{'A','B','C','E'},
+			                      {'S','F','C','S'},
+			                      {'A','D','E','E'}};
+	    String w = "ABCB";
+	    System.out.println(exist(A, w));
+	}
+}
